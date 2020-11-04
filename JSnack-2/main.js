da63fd2c592b2ad40847f11671642b6192c20c5b
Stanislao <stanislaoweb@libero.it>
2020-11-04 14:37:54 +0100

    Inizio Snack

---
+++ JSnack-2/main.js
@@ -0,0 +1,1 @@
+ // Creare un array di nomi e chiedere all’utente due numeri. Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall’utente.
---
+++ JSnack-3/main.js
@@ -0,0 +1,1 @@
+ // A partire da un array di oggetti, creare una copia dell’array e aggiungere ai singoli elementi dell’array una nuova proprietà “position” che contiene una lettera casuale.
