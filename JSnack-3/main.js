67e1d54baea217fb4b08bab68b4186d50f78f8c6
Stanislao <stanislaoweb@libero.it>
2020-11-04 17:11:55 +0100

    Inizio Snack 3, creato array e .map

--- JSnack-3/main.js
+++ JSnack-3/main.js
@@ -1,1 +1,45 @@
  // A partire da un array di oggetti, creare una copia dell’array e aggiungere ai singoli elementi dell’array una nuova proprietà “position” che contiene una lettera casuale.
+
+
+$(document).ready(function() {
+
+  const firstArray = [
+
+    {
+      type: 'car',
+      model: 'i20',
+      brand: 'Hyundai'
+    },
+
+    {
+      type: 'car',
+      model: 'rio',
+      brand: 'Kia'
+    },
+
+    {
+      type: 'car',
+      model: 'panda',
+      brand: 'Fiat'
+    },
+
+    {
+      type: 'car',
+      model: '500',
+      brand: 'Fiat'
+    },
+
+    {
+      type: 'car',
+      model: 'Model X',
+      brand: 'Tesla'
+    },
+  ]
+
+
+  const secondArray  = firstArray.map((element, index)) => {
+
+    element.position = 
+  }
+
+});
